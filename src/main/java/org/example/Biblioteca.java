7d4246c8b372277621e310a409bcf07fbf63e1e9
Nicolas Toledo <nico.toledo.olivero@gmail.com>
2024-10-10 23:43:13 -0300

    Se Agregan metodos: agregarJuego,eliminarJuego y obtenerJuego
    Se añaden los métodos fundamentales para la función de la clase biblioteca

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -1,3 +1,22 @@
 package org.example;
+import java.util.ArrayList;
 public class Biblioteca {
+    private ArrayList<Juego> juegos;
+
+    public Biblioteca() {
+        this.juegos = new ArrayList<>();
+    }
+    public void  agregarJuego(Juego juego) {
+        juegos.add(juego);
+        System.out.println("Juego agregado a la biblioteca: " + juego.getTitulo());
+    }
+
+    public void eliminarJuego(Juego juego) {
+        juegos.remove(juego);
+        System.out.println("main.java.org.example.Juego eliminado de la biblioteca: " + juego.getTitulo());
+    }
+
+    public ArrayList<Juego> obtenerJuegos() {
+        return juegos;
+    }
 }
