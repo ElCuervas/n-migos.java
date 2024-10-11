f321a948362498fe793cf2913d904bfe5ffc85ce
PvllvKuxal <j.contreras26@ufromail.cl>
2024-10-11 12:51:13 -0300

    Cambio(Funcionamiento): Arreglo de métodos y pruebas de funcionamiento
    +Se solucionan partes mal hechas del código como la instancia de usuario
    +Se añade pruebas de funcionamiento de clases clave

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,28 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
+        //pruebas catalogo
         Catalogo catalogo = new Catalogo();
         catalogo.mostrarCatalogo();
         catalogo.conseguirJuegos();
         catalogo.conseguirIDs();
         catalogo.conseguirJuegos();
         catalogo.mostrarCatalogo();
+
+        //pruebas usuario y perfil
+        UsuarioRegistrado usuario1 = new UsuarioRegistrado(1,"juanito", "22123");
+        Perfil perfil1 = new Perfil(usuario1);
+        perfil1.mostrarInformacion();
+
+        //pruebas biblioteca
+        Biblioteca biblioteca = new Biblioteca();
+        Juego juego1 = catalogo.getListaJuegos().get(0);
+        Juego juego2 = catalogo.getListaJuegos().get(2);
+        Juego juego3 = catalogo.getListaJuegos().get(6);
+        biblioteca.agregarJuego(juego1);
+        biblioteca.agregarJuego(juego2);
+        biblioteca.agregarJuego(juego3);
+        System.out.println(biblioteca);
+
     }
 }
