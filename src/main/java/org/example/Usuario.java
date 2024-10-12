7da9a8917eca74b50eb49c5a6e5e8217a0e93cc8
ElCuervas <sebastianaillapan017@gmail.com>
2024-10-11 22:43:36 -0300

    Comentarios de clases para java doc
    Se modificaron las clases de Juego, Usuario y UsuarioNoRegistrado para un archivo java doc

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,10 +1,35 @@
 package org.example;
 
+/**
+ * La clase abstracta Usuario define los atributos y comportamientos básicos
+ * que todos los tipos de usuarios deben implementar.
+ * Un usuario tiene un identificador único y puede buscar juegos o filtrarlos.
+ */
 public abstract class Usuario {
     protected long idUsuario;
+
+    /**
+     * Crea una nueva instancia de Usuario con un identificador único.
+     *
+     * @param idUsuario el identificador único del usuario
+     */
     public Usuario(long idUsuario) {
         this.idUsuario = idUsuario;
     }
+
+    /**
+     * Busca un juego por su nombre. Este método debe ser implementado por las
+     * clases que extienden de Usuario.
+     *
+     * @param nombre el nombre del juego a buscar
+     */
     public abstract void buscarJuegoPorNombre(String nombre);
+
+    /**
+     * Filtra los juegos según un criterio específico. Este método debe ser
+     * implementado por las clases que extienden de Usuario.
+     *
+     * @param filtro el criterio utilizado para filtrar los juegos
+     */
     public abstract void filtrarJuegos(String filtro);
 }
